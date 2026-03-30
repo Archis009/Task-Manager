8e55b312e00f902677d9b0c25bcbcd919613b38b
Archis009 <vedvadnere@gmail.com>
2026-03-30 15:44:48 +0530

    added signup and login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ import { signup, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
+// signup and login routes
 router.post('/signup', signup);
 router.post('/login', login);
 
